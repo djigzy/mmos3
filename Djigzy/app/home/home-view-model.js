e7c5a1535c532ae8e28cb4e72f83878043559c9f
Igor Petrovic <ri.igorpetrovic@gmail.com>
2023-01-21 18:21:33 +0100

    first commit

---
+++ Djigzy/app/home/home-view-model.js
@@ -0,0 +1,18 @@
+import { fromObject, Observable } from '@nativescript/core'
+
+export function HomeViewModel() {
+  const viewModel = new Observable()
+  viewModel.x = 10
+  viewModel.y = 20
+  viewModel.z = 30
+
+  let accelerometer = require("@triniwiz/nativescript-accelerometer")
+  accelerometer.startAccelerometerUpdates(function(data) {
+    viewModel.set('x', data.x)
+    viewModel.set('y', data.y)
+    viewModel.set('z', data.z)
+
+  }, { sensorDelay: "ui" })
+
+  return viewModel
+}
